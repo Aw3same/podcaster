c704f9faaf6b35ca6214c8207b10875a38d52176
Ángel Barroso Sanz <angel.barroso@linguaserve.com>
2024-10-02 13:45:58 +0200

    build: install react router, tailwind and auxiliar dev dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from 'vite-tsconfig-paths'
+
 
 export default defineConfig(({ mode }) => {
-  const isProduction = mode === 'production'
-  
+  const isProduction = mode === "production";
+
   return {
-    plugins: [react()],
+    plugins: [react(), tsconfigPaths()],
     build: {
       minify: isProduction,
       sourcemap: !isProduction,
     },
     css: {
       devSourcemap: !isProduction,
-    }
-  }
-})
+    },
+  };
+});
