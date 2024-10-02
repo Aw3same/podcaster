0cb952b1bf13c3f4584da2576f6ce8538348d0f6
Ángel Barroso Sanz <angel.barroso@linguaserve.com>
2024-10-02 11:53:40 +0200

    build: configure develop and production mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production'
+  
+  return {
+    plugins: [react()],
+    build: {
+      minify: isProduction,
+      sourcemap: !isProduction,
+    },
+    css: {
+      devSourcemap: !isProduction,
+    }
+  }
 })
-
